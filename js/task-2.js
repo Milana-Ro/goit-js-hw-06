e342311322515b546c63e9e6901166ef84dd1fa2
Milana-Ro <milana.roslyak@gmail.com>
2024-02-18 16:27:28 +0200

    made code improvements

--- js/task-2.js
+++ js/task-2.js
@@ -5,18 +5,29 @@ class Storage {
   constructor(items) {
     this.#items = items;
   }
+
   getItems() {
     return this.#items;
   }
+
   addItem(newItem) {
-    this.#items.push(newItem);
+    this.#items = [...this.#items, newItem];
   }
+
+  // addItem(newItem) {
+  //   this.#items.push(newItem);
+  // }
+
   removeItem(itemToRemove) {
-    const itemIndex = this.#items.indexOf(itemToRemove);
-    if (itemIndex !== -1) {
-      this.#items.splice(itemIndex, 1);
-    }
+    this.#items = this.#items.filter((element) => element !== itemToRemove);
   }
+
+  // removeItem(itemToRemove) {
+  //   const itemIndex = this.#items.indexOf(itemToRemove);
+  //   if (itemIndex !== -1) {
+  //     this.#items.splice(itemIndex, 1);
+  //   }
+  // }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
